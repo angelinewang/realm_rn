6d0311c0b1c580f6345abf033372d86f121bf208
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2023-03-14 14:39:12 +0000

    Styling Settings Modal Delete Account/Cancel Button

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,9 +1,15 @@
 import Modal from 'react-native-modal'
 import { Button, Alert, Pressable } from 'react-native'
 import { deleteAccountButtonPress } from '../../analytics.native';
-import { Text } from '../../components/Themed';
+import { Text, View } from '../../components/Themed';
+
+import { useFonts } from 'expo-font';
 
 export default function SettingsModal({ isModalVisible, handleModal, setIsModalVisible }) {
+
+    const [fontsLoaded] = useFonts({
+        'Mulish-Bold': require('../assets/fonts/Mulish-Bold.ttf'),
+    })
     //Add Delete Account Function
   const deleteAccount = async () => {
     try {
@@ -34,11 +40,17 @@ const deleteAccountButton = async () => {
 
     //Display Modal with Button to invoke Delete Account Function
     return (
-        <Modal isVisible={isModalVisible}>
-            <Button title="Delete Account" onPress={deleteAccount}/>
+        <Modal isVisible={isModalVisible} >
+            <View style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', backgroundColor: '#FFFFFF00', height: '15%'}}>
+            <Pressable onPress={deleteAccount} style={{backgroundColor: '#ffffff', width: 335, height: 50, borderRadius: 15, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{color: '#1B1B22', fontSize: 16, fontFamily: 'Mulish-Bold', alignSelf: 'center'}}>Delete Account</Text>
+            </Pressable>
+
             <Pressable onPress={handleModal}>
                 <Text>Cancel</Text>
             </Pressable>
+            </View>
+
         </Modal>
     )
 }
