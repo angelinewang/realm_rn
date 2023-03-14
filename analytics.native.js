b8fbbd33091160a0eeae656b78c2386b10c9c772
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2023-03-14 12:28:51 +0000

    Temporarily commenting out import of react-native-firebase/analytics and commenting out the async functions using the module solves the invariant violation issue when attempting Expo Go

--- analytics.native.js
+++ analytics.native.js
@@ -1,121 +1,121 @@
-import analytics from "@react-native-firebase/analytics";
+// import analytics from "@react-native-firebase/analytics";
 
 //Screen Views
 export async function loginScreenView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "Login",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "Login",
+  // });
 }
 
 export async function signupScreenView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "Signup",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "Signup",
+  // });
 }
 
 export async function profileScreenView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "Profile",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "Profile",
+  // });
 }
 
 export async function invitedScreenView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "Invited",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "Invited",
+  // });
 }
 
 export async function confirmedScreenView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "Confirmed",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "Confirmed",
+  // });
 }
 
 export async function browseScreenView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "Browse",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "Browse",
+  // });
 }
 
 export async function guestlistScreenView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "Guestlist",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "Guestlist",
+  // });
 }
 
 export async function partyModalView() {
-  await analytics().logEvent("specific_screen_view", {
-    screen_name: "PartyModal",
-  });
+  // await analytics().logEvent("specific_screen_view", {
+  //   screen_name: "PartyModal",
+  // });
 }
 
 //Button Presses
 //TO-BE-FINISHED: Add all buttons that exist in the application
 //Login Page
 export async function loginButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "Login/Login",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "Login/Login",
+  // });
 }
 export async function createAccountButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "CreateAccount/Login",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "CreateAccount/Login",
+  // });
 }
 
 //Signup Page
 export async function submitAccountButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "SubmitAccount/Signup",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "SubmitAccount/Signup",
+  // });
 }
 export async function backToLoginButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "BackToLogin/Signup",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "BackToLogin/Signup",
+  // });
 }
 
 //Profile Page
 export async function logoutButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "Logout/Profile",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "Logout/Profile",
+  // });
 }
 export async function deleteAccountButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "DeleteAccount/Profile",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "DeleteAccount/Profile",
+  // });
 }
 export async function settingsButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "Settings/Profile",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "Settings/Profile",
+  // });
 }
 
 //Guests Page
 export async function addPartyButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "AddParty/Guests",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "AddParty/Guests",
+  // });
 }
 
 //Add Party Modal
 export async function submitPartyButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "SubmitParty/AddParty",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "SubmitParty/AddParty",
+  // });
 }
 
 //Browse Page
 export async function inviteButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "Invite/Browse",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "Invite/Browse",
+  // });
 }
 
 //Invited Page
 export async function confirmButtonPress() {
-  await analytics().logEvent("button_press", {
-    button_name: "Confirm/Invited",
-  });
+  // await analytics().logEvent("button_press", {
+  //   button_name: "Confirm/Invited",
+  // });
 }
